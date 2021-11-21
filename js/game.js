e89642df49e4bd2fb8911ac8cff308b808f1bfa6
Peter_Polyakov <73875354+PeterPolyakov@users.noreply.github.com>
2021-11-21 21:48:58 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
 			top: ${this.y}px;
 			width: ${this.width}px;
 			height: ${this.height}px;
-			background: brown;
+			background: orange;
 			`;
 
 			document.body.append(result);
